0cbbd612e24bb1a3af3f2cb43b42aa091f8a1926
Gvenreed <47279866+Gvenreed@users.noreply.github.com>
2019-10-18 21:27:00 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -13,36 +13,15 @@ struct Student
 	string Name;
 };
 
-//void sNm()
-//{
-//
-//}
-//void sGd()
-//{
-//
-//}
-//void sNm()
-//{
-//
-//}
-
-
 int main()
 {
-	Student first [100];
+	string dq;
+	setlocale(LC_ALL, "");
+
+	Student first[100];
 
 	for (int i = 0; i < 100; ++i)
 	{
-		
-	}
-
-	for (int i = 0; i < 100; ++i)
-	{
-		
-	}
-
-	for (int i = 0; i < 100; ++i)
-	{		
 		int lenght = GetRandomValue(3, 6);
 		char letter = GetRandomValue('A', 'Z');
 		first[i].Name += letter;
@@ -54,4 +33,51 @@ int main()
 		}
 	}
 
-}
+	cout << "Введите категорию сортировки (Id, Grade, Name): " << endl;
+	cin >> dq;
+
+	if (dq == "Id")
+	{
+		for (int j = 0; j < 100; j++)
+		{
+			for (int g = 0; g < 99; g++)
+			{
+				if (first[g].Id > first[g + 1].Id)
+				{
+					swap(first[g].Id, first[g + 1].Id);
+				}
+			}
+		}
+	}
+	else if (dq == "Grade")
+	{
+		for (int j = 0; j < 100; j++)
+		{
+			for (int g = 0; g < 99; g++)
+			{
+				if (first[g].Grade > first[g + 1].Grade)
+				{
+					swap(first[g].Grade, first[g + 1].Grade);
+				}
+			}
+		}
+	}
+	else if (dq == "Name")
+	{
+
+		for (int j = 0; j < 100; j++)
+		{
+			for (int g = 0; g < 99; g++)
+			{
+				if (first[g].Name > first[g + 1].Name)
+				{
+					swap(first[g].Name, first[g + 1].Name);
+				}
+			}
+		}
+	}
+	else
+	{
+		cout << "ошибка ввода" << endl;
+	}
+};
