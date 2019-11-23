bb58d84b7c9a54db50c1d91085857866005093e5
Gvenreed <47279866+Gvenreed@users.noreply.github.com>
2019-11-23 21:07:34 +0300

    Create charString.cpp

---
+++ charString.cpp
@@ -0,0 +1,39 @@
+#include "charString.hpp"
+#include <iostream>
+
+using namespace std;
+
+namespace ext
+{
+	void append(charString*& str, char symbol)
+	{
+		charString* ptr = new charString{ symbol };
+		
+		if (str == nullptr)
+		{
+			str = ptr;
+			return;
+		}
+
+		charString* temp = str;
+
+		while (temp-> nextSymbol != nullptr)
+		{
+			temp = temp->nextSymbol;
+		}
+		temp->nextSymbol = ptr;
+	}
+	void characterAt(charString* str, int num)
+	{
+		int temp;
+
+	}
+	void print(charString* str)
+	{
+		while (str != nullptr)
+		{
+			cout << str->symbol;
+			str = str->nextSymbol;
+		}
+	}
+}
