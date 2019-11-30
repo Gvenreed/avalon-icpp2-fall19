fdd4b237e7705d67ce8432f22f9d11c87be48f9e
Gvenreed <47279866+Gvenreed@users.noreply.github.com>
2019-11-30 16:48:53 +0300

    Update CharArray.cpp

--- CharArray.cpp
+++ CharArray.cpp
@@ -57,14 +57,75 @@ namespace ext
 		delete[] first;
 		first = result;
 	}
+
 	void substring(charArray& first, int p, int l)
 	{
-		
+
 		charArray result = new char[l];
 		for (int i = p; i < p + l; ++i)
 		{
 			result[i - p] = first[i];
 		}
 		result[l] = '/0';
+	}
+
+	void replaceString(charArray& first, charArray& second, charArray& third)
+	{
+		int lengthFirst = countLength(first);
+		int lengthSecond = countLength(second);
+		int lengthThird = countLength(third);
+
+		int num[16];
+		int res = 0;
+		int k = 0;
+		int dq = 0;
+		for (int i = 0; i < lengthFirst; i++)
+		{
+			int temp = 0;
+			k = lengthSecond * i;
+			for (int j = 0; j < lengthSecond; j++)
+			{
+				if (first[k] == second[j])
+				{
+					temp += 1;
+				}
+				else
+				{
+					temp = 0;					
+				}
+				k += 1;
+			}
+			if (temp == lengthSecond)
+			{
+				res += 1;
+				num[dq] = k;
+				dq += 1;
+			}
+		}
+		charArray result = new char[lengthFirst + res * (-lengthSecond +lengthThird)-1];
+		int g = 0;
+		int rr = 0;
+		int u;
+		for (int i = 0; i < lengthFirst + res * (-lengthSecond +lengthThird); ++i)
+		{
+			
+			if ((i + lengthSecond) == num[rr])
+			{
+				for (int j = 0; j < lengthThird; ++j)
+				{
+					result[g] = third[j];
+					g += 1;
+				}
+				rr += 1;
+				u = 0;
+			}
+			else
+			{
+				result[g] = first[num[rr - 1]+u+rr-1];
+				g += 1;
+				u += 1;
+			}
+		}
+		result[g] = '\0';
 	}
 }
