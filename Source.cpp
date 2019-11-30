f3309618cebc2554d3eb3bded5d11d96817fc38c
Gvenreed <47279866+Gvenreed@users.noreply.github.com>
2019-11-30 16:49:48 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "CharArray.hpp"
 
 using namespace ext;
@@ -7,13 +8,18 @@ int main()
 	charArray str = new char[6]{ "hello" };
 	charArray str2 = new char[7]{ " world" };
 	char str3 = '!';
+	charArray str4 = new char[17]{ "aabbaaccaaddaaee" };
+	charArray str5 = new char[3]{ "aa" };
+	charArray str6 = new char[2]{ "a" };
+	
 	int pos, len;
-	pos = 2;
-	len = 3;
+	std::cin >> pos;
+	std::cin >> len;
 	char result1 = characterAt(str, 1); // h
 	char result2 = characterAt(str, 3); // l
 
 	concatenate(str, str2); // str -> "hello world"
 	append(str, str3);
 	substring(str, pos, len);
+	replaceString(str4, str5, str6);
 }
