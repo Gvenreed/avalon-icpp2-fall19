81753e4ac696ab3a7f07dbb429d9e8638551d8a8
Gvenreed <47279866+Gvenreed@users.noreply.github.com>
2019-12-07 16:30:31 +0300

    Update charString.hpp

--- charString.hpp
+++ charString.hpp
@@ -9,4 +9,7 @@ namespace ext
 	void characterAt(charString* str, int num);
 	void append(charString* & str, char symbol);
 	void print(charString* str);
+	void removeChars(charString* str, int count, int position);
+	void concatenate(charString*& str1, charString*& str2);
+	void substring(charString*& str, int pos, int len);
 }
