59609ef09ad52cabb9126eb349d0574006513021
Gvenreed <47279866+Gvenreed@users.noreply.github.com>
2019-12-07 16:30:13 +0300

    Update charString.cpp

--- charString.cpp
+++ charString.cpp
@@ -8,7 +8,7 @@ namespace ext
 	void append(charString*& str, char symbol)
 	{
 		charString* ptr = new charString{ symbol };
-		
+
 		if (str == nullptr)
 		{
 			str = ptr;
@@ -17,7 +17,7 @@ namespace ext
 
 		charString* temp = str;
 
-		while (temp-> nextSymbol != nullptr)
+		while (temp->nextSymbol != nullptr)
 		{
 			temp = temp->nextSymbol;
 		}
@@ -25,11 +25,13 @@ namespace ext
 	}
 	void characterAt(charString* str, int num)
 	{
-		int temp;
-		while (temp != num)
+		int pos = 0;
+		charString* temp = str;
+		while (pos != num)
 		{
-			
-		}
+			temp = temp->nextSymbol;
+			pos += 1;
+		}	
 	}
 	void print(charString* str)
 	{
@@ -37,6 +39,56 @@ namespace ext
 		{
 			cout << str->symbol;
 			str = str->nextSymbol;
+		}
+	}
+	void removeChars(charString* str, int count, int position)
+		{
+			charString* savedPtr = str;
+
+			int currentPos = 1;
+			while (currentPos != position - 1)
+			{
+				str = str->nextSymbol;
+				++currentPos;
+			}
+
+			for (int i = 1; i <= count; ++i)
+			{
+				charString* temp = str->nextSymbol;
+				str->nextSymbol = temp->nextSymbol;
+				delete temp;
+			}
+		}
+	void concatenate(charString*& str1, charString*& str2)
+	{
+		charString* temp = str1;
+		charString* temp2 = str2;
+
+		while (temp->nextSymbol != nullptr)
+		{
+			temp = temp->nextSymbol;
+		}
+		while (temp2->nextSymbol != nullptr)
+		{
+			temp->nextSymbol = temp2;
+			temp2 = temp2->nextSymbol;
+		}
+	}
+	void substring(charString*& str, int pos, int len)
+	{
+		charString* saved = str;
+		charString* temp = nullptr;
+		int i = 0;
+		while (i != pos)
+		{
+			saved = saved->nextSymbol;
+			i += 1;
+		}
+		temp = saved;
+		for (int j=1; j < len-1; ++j)
+		{
+			temp->nextSymbol = saved;
+			saved = saved->nextSymbol;
 		}
 	}
 }
